377fb57a37a598abda8d9f3a7983c861d52462f9
Alex Widener <awidener3@gmail.com>
2022-03-20 14:09:09 -0400

    Finished setting up basic routing using react router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 // import Card from './components/Card';
-// import Home from './pages/Home';
-// import Battle from './pages/Battle';
-// import CreateBattle from './pages/CreateBattle';
+import Home from './pages/Home';
+import Battle from './pages/Battle';
+import CreateBattle from './pages/CreateBattle';
 import './App.scss';
-
-function Home() {
-	return (
-		<>
-			<main>
-				<h2>Welcome to the homepage!</h2>
-				<p>You can do this, I believe in you.</p>
-			</main>
-			<nav>
-				<Link to="/about">About</Link>
-			</nav>
-		</>
-	);
-}
-
-function About() {
-	return (
-		<>
-			<main>
-				<h2>Who are we?</h2>
-				<p>That feels like an existential question, don't you think?</p>
-			</main>
-			<nav>
-				<Link to="/">Home</Link>
-			</nav>
-		</>
-	);
-}
 
 const App = () => {
 	return (
 		<div className="App">
 			<h1>Welcome to React Router!</h1>
+			<Link to="/dm-deck">
+				<button type="button" className="btn btn-primary m-2">
+					Home
+				</button>
+			</Link>
+			<Link to="/dm-deck/battle">
+				<button type="button" className="btn btn-primary m-2">
+					Battle
+				</button>
+			</Link>
+			<Link to="/dm-deck/create-battle">
+				<button type="button" className="btn btn-primary m-2">
+					Create Battle
+				</button>
+			</Link>
 			<Routes>
-				<Route exact path="/" element={<Home />} />
-				<Route path="about" element={<About />} />
+				<Route path="/dm-deck" element={<Home />} />
+				<Route path="/dm-deck/battle" element={<Battle />} />
+				<Route
+					path="/dm-deck/create-battle"
+					element={<CreateBattle />}
+				/>
 			</Routes>
 		</div>
 	);
