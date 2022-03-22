c20f1bd8a14354d87567b9a37642822ca549d340
Alex Widener <awidener3@gmail.com>
2022-03-22 14:06:22 -0400

    added carousel to battle cards

--- src/pages/Battle.jsx
+++ src/pages/Battle.jsx
@@ -1,14 +1,36 @@
 import React, { useEffect, useState } from 'react';
+
 import Card from '../components/Card';
 import Hero from '../components/Hero';
+
 import monsters from '../components/Card/cardData';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import '../App.scss';
 
 const Battle = () => {
 	const [monsterData, setMonsterData] = useState(monsters);
+	const [index, setIndex] = useState(0);
 
 	useEffect(() => {
 		addInitiative();
 	}, []);
+
+	const slideLeft = () => {
+		if (index - 1 >= 0) {
+			setIndex(index - 1);
+		}
+	};
+
+	const slideRight = () => {
+		console.log(index, monsters.length);
+		if (index + 1 <= monsters.length - 1) {
+			setIndex(index + 1);
+		}
+
+		if (index === monsters.length - 1) {
+			setIndex(0);
+		}
+	};
 
 	const getInitiative = (monster) => {
 		return (
@@ -29,16 +51,33 @@ const Battle = () => {
 		const sortedData = [].concat(monsterData);
 		return sortedData
 			.sort((a, b) => (a.initiative < b.initiative ? 1 : -1))
-			.map((monster, index) => <Card key={index} monster={monster} />);
+			.map((monster, n) => {
+				let position =
+					n > index
+						? 'nextCard'
+						: n === index
+						? 'activeCard'
+						: 'prevCard';
+				return (
+					<Card
+						key={monster.name}
+						monster={monster}
+						cardStyle={position}
+					/>
+				);
+			});
 	};
 
 	return (
 		<div className="p-4 d-flex flex-column justify-content-center align-items-center container">
 			<h1>Example Battle</h1>
 
-			<Hero />
+			{/* <Hero /> */}
+			<FaChevronLeft onClick={slideLeft} className="leftBtn" />
+			<FaChevronRight onClick={slideRight} className="rightBtn" />
 
-			<div className="card-container">
+			<div className="card-container container d-flex justify-content-center">
+				<div className="background-block"></div>
 				{monsterData[0].initiative ? renderCards() : null}
 			</div>
 		</div>
