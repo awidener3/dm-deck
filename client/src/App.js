206bf181e1b94834df397cbb286640399fb73923
Alex Widener <awidener3@gmail.com>
2022-03-24 14:54:59 -0400

    Added apollo server starter files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+import './App.scss';
+
+const App = () => {
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
+};
+
+export default App;
---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from '../../pages/Home';
+import Battle from '../../pages/Battle';
+import CreateBattle from '../../pages/CreateBattle';
+
+// ! removed "homepage": "https://awidener3.github.io/dm-deck/", from package.json
+
+const Header = () => {
+	return (
+		<div>
+			<nav className="navbar navbar-expand bg-dark text-light">
+				<div className="container">
+					<h1 className="navbar-brand">🐲 DM Deck</h1>
+					<ul className="navbar-nav">
+						<Link to="/" className="nav-link m-2">
+							Home
+						</Link>
+						<Link to="/battle" className="nav-link m-2">
+							Battle
+						</Link>
+						<Link to="/create-battle" className="nav-link m-2">
+							Create
+						</Link>
+					</ul>
+				</div>
+			</nav>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/battle" element={<Battle />} />
+				<Route path="/create-battle" element={<CreateBattle />} />
+			</Routes>
+		</div>
+	);
+};
+
+export default Header;
