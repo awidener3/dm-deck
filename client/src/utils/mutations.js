c05d2e414183b7ea3ffe6748580d12227aae81d8
Alex Widener <awidener3@gmail.com>
2022-05-19 20:42:20 -0400

    Added authentication, mutations and queries to client-side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				_id
+			}
+		}
+	}
+`;
+
+export const UPDATE_USER = gql`
+	mutation UpdateUser($email: String!, $password: String!) {
+		updateUser(email: $email, password: $password) {
+			_id
+			username
+		}
+	}
+`;
+
+export const LOGIN = gql`
+	mutation login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+			}
+		}
+	}
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,34 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+	query users {
+		users {
+			_id
+			username
+			email
+			password
+		}
+	}
+`;
+
+export const QUERY_USER = gql`
+	query User($userId: ID!) {
+		user(userId: $userId) {
+			_id
+			username
+			email
+			password
+		}
+	}
+`;
+
+export const QUERY_ME = gql`
+	query me {
+		me {
+			_id
+			username
+			email
+			password
+		}
+	}
+`;
